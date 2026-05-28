d7b79ec302b631449472a829f7e9fe00a1eba0c9
TheBoBa2 <glooplootloo@gmail.com>
2026-05-28 23:08:53 +0300

    added 2 task

---
+++ 2 task.py
@@ -0,0 +1,6 @@
+﻿first_input = float(input("Введіть перше значення: "))
+second_input = float(input("Введіть друге значення: "))
+third_input = float(input("Введіть третє значення: "))
+
+result = (first_input + second_input + third_input) // 3
+print(result)
